c03180d797cadd3789b40d0192c4f32d0bb304e4
jordanjunior22 <jordan.kheambo@final.edu.tr>
2025-01-22 20:39:24 +0100

    update

--- src/utils/generateId.js
+++ src/utils/generateId.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Function to generate a random ID with a custom prefix
 const generateId = (prefix, length = 8) => {
-  const randomNumber = Math.floor(Math.random() * 10000000000) + 1; // Generates a number between 1 and 9,999,999,999
+  const randomNumber = Math.floor(Math.random() * 1000000000000) + 1; // Generate a random number between 1 and 1 trillion (1,000,000,000,000)
   return `${prefix}-${randomNumber.toString().padStart(length, '0')}`; // Format as PREFIX-00000001
 };
 
