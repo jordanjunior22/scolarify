b159403ea4b581a72f14cb3fbef4f91f7508b328
jordanjunior22 <jordan.kheambo@final.edu.tr>
2025-04-29 12:35:09 +0100

    update

--- src/routes/InvitationRoutes.js
+++ src/routes/InvitationRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/get-invitations', authenticate, checkSubscription, authorize(['admin', 'super']), invitationController.getAllInvitations);
 router.get('/get-invitation/:id', authenticate, checkSubscription, authorize(['admin', 'super']), invitationController.getInvitationById);
-// router.post('/create-invitation', authenticate, authorize(['admin', 'super']), invitationController.createInvitation);
+router.post('/create-invitation', authenticate, authorize(['admin', 'super']), invitationController.createInvitation);
 router.put('/update-invitation/:id', authenticate, authorize(['admin', 'super']), invitationController.updateInvitationById);
 router.delete('/delete-invitation/:id', authenticate, authorize(['admin', 'super']), invitationController.deleteInvitationById);
 router.delete('/delete-invitations', authenticate, authorize(['admin', 'super']), invitationController.deleteMultipleInvitations);
